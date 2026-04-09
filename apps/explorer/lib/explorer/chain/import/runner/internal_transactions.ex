138e3de266ec1c7a5f2aa90056285458a8e1a97b
Wolmin <lamonos123@gmail.com>
2026-04-09 12:07:41 +0200

    Fix errors on reindexing

--- apps/explorer/lib/explorer/chain/import/runner/internal_transactions.ex
+++ apps/explorer/lib/explorer/chain/import/runner/internal_transactions.ex
@@ -235,7 +235,7 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
       Import.insert_changes_list(
         repo,
         ordered_changes_list,
-        conflict_target: [:block_hash, :block_index],
+        conflict_target: [:block_hash, :transaction_index, :index],
         for: InternalTransaction,
         on_conflict: on_conflict,
         returning: true,
@@ -251,6 +251,7 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
       internal_transaction in InternalTransaction,
       update: [
         set: [
+          block_index: fragment("EXCLUDED.block_index"),
           block_number: fragment("EXCLUDED.block_number"),
           call_type: fragment("EXCLUDED.call_type"),
           created_contract_address_hash: fragment("EXCLUDED.created_contract_address_hash"),
@@ -259,28 +260,25 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
           from_address_hash: fragment("EXCLUDED.from_address_hash"),
           gas: fragment("EXCLUDED.gas"),
           gas_used: fragment("EXCLUDED.gas_used"),
-          index: fragment("EXCLUDED.index"),
           init: fragment("EXCLUDED.init"),
           input: fragment("EXCLUDED.input"),
           output: fragment("EXCLUDED.output"),
           to_address_hash: fragment("EXCLUDED.to_address_hash"),
           trace_address: fragment("EXCLUDED.trace_address"),
           transaction_hash: fragment("EXCLUDED.transaction_hash"),
-          transaction_index: fragment("EXCLUDED.transaction_index"),
           type: fragment("EXCLUDED.type"),
           value: fragment("EXCLUDED.value"),
           inserted_at: fragment("LEAST(?, EXCLUDED.inserted_at)", internal_transaction.inserted_at),
           updated_at: fragment("GREATEST(?, EXCLUDED.updated_at)", internal_transaction.updated_at)
-          # Don't update `block_hash` as it is used for the conflict target
-          # Don't update `block_index` as it is used for the conflict target
+          # Don't update `block_hash`, `transaction_index`, or `index` as they are used for the conflict target
         ]
       ],
       # `IS DISTINCT FROM` is used because it allows `NULL` to be equal to itself
       where:
         fragment(
-          "(EXCLUDED.transaction_hash, EXCLUDED.index, EXCLUDED.call_type, EXCLUDED.created_contract_address_hash, EXCLUDED.created_contract_code, EXCLUDED.error, EXCLUDED.from_address_hash, EXCLUDED.gas, EXCLUDED.gas_used, EXCLUDED.init, EXCLUDED.input, EXCLUDED.output, EXCLUDED.to_address_hash, EXCLUDED.trace_address, EXCLUDED.transaction_index, EXCLUDED.type, EXCLUDED.value) IS DISTINCT FROM (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          "(EXCLUDED.block_index, EXCLUDED.transaction_hash, EXCLUDED.call_type, EXCLUDED.created_contract_address_hash, EXCLUDED.created_contract_code, EXCLUDED.error, EXCLUDED.from_address_hash, EXCLUDED.gas, EXCLUDED.gas_used, EXCLUDED.init, EXCLUDED.input, EXCLUDED.output, EXCLUDED.to_address_hash, EXCLUDED.trace_address, EXCLUDED.type, EXCLUDED.value) IS DISTINCT FROM (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          internal_transaction.block_index,
           internal_transaction.transaction_hash,
-          internal_transaction.index,
           internal_transaction.call_type,
           internal_transaction.created_contract_address_hash,
           internal_transaction.created_contract_code,
@@ -293,7 +291,6 @@ defmodule Explorer.Chain.Import.Runner.InternalTransactions do
           internal_transaction.output,
           internal_transaction.to_address_hash,
           internal_transaction.trace_address,
-          internal_transaction.transaction_index,
           internal_transaction.type,
           internal_transaction.value
         )
